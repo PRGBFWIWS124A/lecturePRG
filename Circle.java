578899d6e9431569160eae467fae416c7465cb6b
nikolasperingfhdw <nikolas.pering@edu.fhdw.de>
2024-07-05 16:38:59 +0200

    first version, hi

---
+++ Circle.java
@@ -0,0 +1,17 @@
+package battelship.model;
+
+public class Circle {
+
+    private int x;
+    private int y;
+    private int radius;
+
+    public static Circle newCircle(int x, int y, int radius){
+        return
+    }
+
+    public Circle clone(){
+        return
+    }
+
+}
